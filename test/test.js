6acb552a4b6210443bd0134ca37828919cc79e72
Alexander Kuznetsov <sasha@monterosa.co.uk>
2014-08-31 02:05:01 +0100

    Add CSS tests

--- test/test.js
+++ test/test.js
@@ -14,7 +14,9 @@ describe('gulp-minify-html', function ( )
 			.pipe(through.obj(function ( file, e, c ) {
 				var contents = file.contents.toString();
 				expect(contents).to.match(/a=1/);
-				expect(contents).not.to.match(/Comment/);
+				expect(contents).to.match(/body{/);
+				expect(contents).not.to.match(/JSComment/);
+				expect(contents).to.match(/CSSComment/);
 				done();
 			}));
 	});
@@ -26,19 +28,41 @@ describe('gulp-minify-html', function ( )
 			.pipe(through.obj(function ( file, e, c ) {
 				var contents = file.contents.toString();
 				expect(contents).to.match(/a=1/);
-				expect(contents).to.match(/Comment/);
+				expect(contents).to.match(/JSComment/);
 				done();
 			}));
 	});
 
-	it('should be possible to disable JS minification', function ( done )
+	it('should be possible to disable uglify', function ( done )
 	{
 		gulp.src(fixture)
 			.pipe(minifyInline({js: false}))
 			.pipe(through.obj(function ( file, e, c ) {
 				var contents = file.contents.toString();
 				expect(contents).to.match(/a = 1/);
-				expect(contents).to.match(/Comment/);
+				done();
+			}));
+	});
+
+	it('should use clean-css options', function ( done )
+	{
+		gulp.src(fixture)
+			.pipe(minifyInline({css: {keepSpecialComments: 0}}))
+			.pipe(through.obj(function ( file, e, c ) {
+				var contents = file.contents.toString();
+				expect(contents).to.match(/body{/);
+				expect(contents).not.to.match(/CSSComment/);
+				done();
+			}));
+	});
+
+	it('should be possible to disable clean-css', function ( done )
+	{
+		gulp.src(fixture)
+			.pipe(minifyInline({css: false}))
+			.pipe(through.obj(function ( file, e, c ) {
+				var contents = file.contents.toString();
+				expect(contents).to.match(/body {/);
 				done();
 			}));
 	});
